145405ab47103e15042ea28abbdf7805550c30bf
Riyaj Tamboli <tambolir97@gmail.com>
2025-12-15 15:24:24 +0530

    added one prpgram

---
+++ DSA/src/String_Practice/ConcateString.java
@@ -0,0 +1,13 @@
+package String_Practice;
+
+public class ConcateString {
+
+	public static void main(String[] args) {
+		String name = "Riyaj";
+		String lastName = "Tamboli";
+		String fullName = name + " " +lastName;
+		IO.println(fullName);
+
+	}
+
+}
