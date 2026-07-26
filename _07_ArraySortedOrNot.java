046b309c2edc22c939cd57d996ef920771c9671f
riyajtamboli <tambolir97@gmail.com>
2026-07-26 13:03:09 +0530

    Sorted or not;

---
+++ _07_ArraySortedOrNot.java
@@ -0,0 +1,16 @@
+public class _07_ArraySortedOrNot {
+    void main(){
+        int arr [] = {1,2,3,4,5};
+        System.out.println(sorted(arr, 0));
+    }
+
+    public boolean sorted(int arr[], int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i]+1){
+            return false;
+        }
+       return sorted(arr, i+1);
+    }
+}
