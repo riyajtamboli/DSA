c7fadf91ef7048dba1469fe9fa52dec33981069c
riyajtamboli <tambolir97@gmail.com>
2026-07-26 17:40:35 +0530

    Built in sort

---
+++ Array/_26BuiltInSort.java
@@ -0,0 +1,11 @@
+package Array;
+
+import java.util.Arrays;
+
+public class _26BuiltInSort {
+    void main(){
+        int arr[] = {5, 1, 4, 2, 8};
+        Arrays.sort(arr);
+        IO.println(Arrays.toString(arr));
+    }
+}
