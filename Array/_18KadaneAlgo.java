14243346b8be18bd544e56a3986fd9c1d75b4869
riyajtamboli <tambolir97@gmail.com>
2026-07-26 15:08:26 +0530

    KadaneAlgo added

---
+++ Array/_18KadaneAlgo.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class _18KadaneAlgo {
+    void main(){
+        int arr [] ={-2,1,-3,4,-1,2,1,-5,4};
+        int currSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i=1; i<arr.length; i++){
+            currSum = Math.max(arr[i], currSum+arr[i]);
+            maxSum= Math.max(maxSum, currSum);
+        }
+
+        System.out.println(maxSum);
+    }
+
+
+}
