692667e5e4acf9b6e1adc70746af11aa75114dc5
riyajtamboli <tambolir97@gmail.com>
2026-07-26 22:35:48 +0530

    Print duplicate

---
+++ Strings/_07DuplicateChar.java
@@ -0,0 +1,17 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class _07DuplicateChar {
+    void main(){
+        String str = "programming";
+        HashSet<Character> hs = new HashSet<>();
+        for(int i=0; i<str.length(); i++){
+            if(!hs.add(str.charAt(i))){
+                IO.print(str.charAt(i)+" ");
+            }
+        }
+
+    }
+}
