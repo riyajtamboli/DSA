71db77d57511e9a60fc522aa405652691a830efd
riyajtamboli <tambolir97@gmail.com>
2026-08-02 21:47:54 +0530

    Number Pattern

---
+++ _06Pattern/_04NumberPattern.java
@@ -0,0 +1,13 @@
+package _06Pattern;
+
+public class _04NumberPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
